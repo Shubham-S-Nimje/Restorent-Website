44c59971faba598f92a9cb860fbc2717402d0135
shubhamnimje77@gmail.com <shubhamnimje77@gmail.com>
2023-03-09 16:16:32 +0530

    mealsummery added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Contextdata from './components/Context/Contextdata';
 import Header from './components/Layout/Header'
+import Mealsummery from './components/Meals/Mealsummery';
 
 function App() {
   return (
     <Contextdata.Provider value=''>
       <Header/>
+      <Mealsummery/>
     </Contextdata.Provider>
   );
 }
